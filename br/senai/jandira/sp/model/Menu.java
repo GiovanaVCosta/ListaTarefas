0e467c77a26963a0291b7da3aaba7f902cb81b80
Giovana <23111728@CFP127ALUNO.LOCAL>
2023-09-13 16:27:19 -0300

    tarefas

---
+++ br/senai/jandira/sp/model/Menu.java
@@ -0,0 +1,24 @@
+package br.senai.jandira.sp.model;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void main(String[] args) {
+
+//nao sei fazer `-´
+
+        Scanner teclado = new Scanner(System.in);
+
+
+
+        while (true) {
+            System.out.println("++++++++++++++++++++++++++++++++");
+            System.out.println("[ 1 | Adicionar uma nova tarefa]");
+            System.out.println("[ 2 |     Editar uma tarefa    ]");
+            System.out.println("[ 3 |    Excluir uma tarefa    ]");
+
+            int escolha = teclado.nextInt();
+        }
+        }
+    }
